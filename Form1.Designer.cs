8555556ddb64109ce0d02dcb6f2d2371c81d0b16
alireza <alreka1379@gmail.com>
2020-10-09 09:41:59 +0330

    first step of made .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,13 +44,21 @@
             this.btnnext = new System.Windows.Forms.Button();
             this.btnlast = new System.Windows.Forms.Button();
             this.btnfirst = new System.Windows.Forms.Button();
+            this.comboBoxsearched = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxsearchfor = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnsearch = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.label1.Location = new System.Drawing.Point(22, 33);
+            this.label1.Location = new System.Drawing.Point(33, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(73, 29);
             this.label1.TabIndex = 0;
@@ -59,17 +67,19 @@
             // 
             // textBoxname
             // 
-            this.textBoxname.Location = new System.Drawing.Point(141, 40);
+            this.textBoxname.Location = new System.Drawing.Point(212, 62);
+            this.textBoxname.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxname.Name = "textBoxname";
-            this.textBoxname.Size = new System.Drawing.Size(199, 22);
+            this.textBoxname.Size = new System.Drawing.Size(296, 30);
             this.textBoxname.TabIndex = 1;
             this.textBoxname.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBoxphone
             // 
-            this.textBoxphone.Location = new System.Drawing.Point(141, 122);
+            this.textBoxphone.Location = new System.Drawing.Point(212, 191);
+            this.textBoxphone.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxphone.Name = "textBoxphone";
-            this.textBoxphone.Size = new System.Drawing.Size(199, 22);
+            this.textBoxphone.Size = new System.Drawing.Size(296, 30);
             this.textBoxphone.TabIndex = 5;
             this.textBoxphone.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
@@ -77,7 +87,8 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.label3.Location = new System.Drawing.Point(22, 115);
+            this.label3.Location = new System.Drawing.Point(33, 180);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(81, 29);
             this.label3.TabIndex = 4;
@@ -86,10 +97,11 @@
             // 
             // textBoxaddress
             // 
-            this.textBoxaddress.Location = new System.Drawing.Point(141, 162);
+            this.textBoxaddress.Location = new System.Drawing.Point(212, 253);
+            this.textBoxaddress.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxaddress.Multiline = true;
             this.textBoxaddress.Name = "textBoxaddress";
-            this.textBoxaddress.Size = new System.Drawing.Size(199, 92);
+            this.textBoxaddress.Size = new System.Drawing.Size(296, 142);
             this.textBoxaddress.TabIndex = 7;
             this.textBoxaddress.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
@@ -97,7 +109,8 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.label4.Location = new System.Drawing.Point(22, 155);
+            this.label4.Location = new System.Drawing.Point(33, 242);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(100, 29);
             this.label4.TabIndex = 6;
@@ -106,9 +119,10 @@
             // 
             // textBoxfamily
             // 
-            this.textBoxfamily.Location = new System.Drawing.Point(141, 80);
+            this.textBoxfamily.Location = new System.Drawing.Point(212, 125);
+            this.textBoxfamily.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxfamily.Name = "textBoxfamily";
-            this.textBoxfamily.Size = new System.Drawing.Size(199, 22);
+            this.textBoxfamily.Size = new System.Drawing.Size(296, 30);
             this.textBoxfamily.TabIndex = 9;
             this.textBoxfamily.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
@@ -116,7 +130,8 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.label5.Location = new System.Drawing.Point(22, 73);
+            this.label5.Location = new System.Drawing.Point(33, 114);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(75, 29);
             this.label5.TabIndex = 8;
@@ -125,9 +140,10 @@
             // btnnew
             // 
             this.btnnew.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.btnnew.Location = new System.Drawing.Point(48, 300);
+            this.btnnew.Location = new System.Drawing.Point(15, 469);
+            this.btnnew.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnnew.Name = "btnnew";
-            this.btnnew.Size = new System.Drawing.Size(93, 50);
+            this.btnnew.Size = new System.Drawing.Size(140, 78);
             this.btnnew.TabIndex = 10;
             this.btnnew.Text = "new";
             this.btnnew.UseVisualStyleBackColor = true;
@@ -135,9 +151,10 @@
             // btndel
             // 
             this.btndel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.btndel.Location = new System.Drawing.Point(416, 300);
+            this.btndel.Location = new System.Drawing.Point(460, 469);
+            this.btndel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btndel.Name = "btndel";
-            this.btndel.Size = new System.Drawing.Size(93, 50);
+            this.btndel.Size = new System.Drawing.Size(140, 78);
             this.btndel.TabIndex = 12;
             this.btndel.Text = "del";
             this.btndel.UseVisualStyleBackColor = true;
@@ -145,9 +162,10 @@
             // btnedit
             // 
             this.btnedit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.btnedit.Location = new System.Drawing.Point(304, 300);
+            this.btnedit.Location = new System.Drawing.Point(312, 469);
+            this.btnedit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnedit.Name = "btnedit";
-            this.btnedit.Size = new System.Drawing.Size(93, 50);
+            this.btnedit.Size = new System.Drawing.Size(140, 78);
             this.btnedit.TabIndex = 13;
             this.btnedit.Text = "edit";
             this.btnedit.UseVisualStyleBackColor = true;
@@ -155,9 +173,10 @@
             // btnsave
             // 
             this.btnsave.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.btnsave.Location = new System.Drawing.Point(178, 300);
+            this.btnsave.Location = new System.Drawing.Point(164, 469);
+            this.btnsave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnsave.Name = "btnsave";
-            this.btnsave.Size = new System.Drawing.Size(93, 50);
+            this.btnsave.Size = new System.Drawing.Size(140, 78);
             this.btnsave.TabIndex = 14;
             this.btnsave.Text = "save";
             this.btnsave.UseVisualStyleBackColor = true;
@@ -165,9 +184,10 @@
             // btnprev
             // 
             this.btnprev.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.btnprev.Location = new System.Drawing.Point(304, 372);
+            this.btnprev.Location = new System.Drawing.Point(315, 581);
+            this.btnprev.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnprev.Name = "btnprev";
-            this.btnprev.Size = new System.Drawing.Size(93, 50);
+            this.btnprev.Size = new System.Drawing.Size(140, 78);
             this.btnprev.TabIndex = 18;
             this.btnprev.Text = "prev";
             this.btnprev.UseVisualStyleBackColor = true;
@@ -175,9 +195,10 @@
             // btnnext
             // 
             this.btnnext.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.btnnext.Location = new System.Drawing.Point(178, 372);
+            this.btnnext.Location = new System.Drawing.Point(166, 581);
+            this.btnnext.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnnext.Name = "btnnext";
-            this.btnnext.Size = new System.Drawing.Size(93, 50);
+            this.btnnext.Size = new System.Drawing.Size(140, 78);
             this.btnnext.TabIndex = 17;
             this.btnnext.Text = "next";
             this.btnnext.UseVisualStyleBackColor = true;
@@ -185,9 +206,10 @@
             // btnlast
             // 
             this.btnlast.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.btnlast.Location = new System.Drawing.Point(416, 372);
+            this.btnlast.Location = new System.Drawing.Point(464, 581);
+            this.btnlast.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnlast.Name = "btnlast";
-            this.btnlast.Size = new System.Drawing.Size(93, 50);
+            this.btnlast.Size = new System.Drawing.Size(140, 78);
             this.btnlast.TabIndex = 16;
             this.btnlast.Text = "last";
             this.btnlast.UseVisualStyleBackColor = true;
@@ -195,18 +217,87 @@
             // btnfirst
             // 
             this.btnfirst.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.btnfirst.Location = new System.Drawing.Point(48, 372);
+            this.btnfirst.Location = new System.Drawing.Point(18, 581);
+            this.btnfirst.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnfirst.Name = "btnfirst";
-            this.btnfirst.Size = new System.Drawing.Size(93, 50);
+            this.btnfirst.Size = new System.Drawing.Size(140, 78);
             this.btnfirst.TabIndex = 15;
             this.btnfirst.Text = "first";
             this.btnfirst.UseVisualStyleBackColor = true;
             // 
+            // comboBoxsearched
+            // 
+            this.comboBoxsearched.FormattingEnabled = true;
+            this.comboBoxsearched.Location = new System.Drawing.Point(904, 52);
+            this.comboBoxsearched.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBoxsearched.Name = "comboBoxsearched";
+            this.comboBoxsearched.Size = new System.Drawing.Size(276, 33);
+            this.comboBoxsearched.TabIndex = 19;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(645, 52);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 29);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "searched by:";
+            // 
+            // textBoxsearchfor
+            // 
+            this.textBoxsearchfor.Location = new System.Drawing.Point(904, 125);
+            this.textBoxsearchfor.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxsearchfor.Name = "textBoxsearchfor";
+            this.textBoxsearchfor.Size = new System.Drawing.Size(276, 30);
+            this.textBoxsearchfor.TabIndex = 22;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label6.Location = new System.Drawing.Point(645, 114);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(153, 29);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "searched for:";
+            // 
+            // btnsearch
+            // 
+            this.btnsearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnsearch.Location = new System.Drawing.Point(652, 180);
+            this.btnsearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnsearch.Name = "btnsearch";
+            this.btnsearch.Size = new System.Drawing.Size(196, 66);
+            this.btnsearch.TabIndex = 23;
+            this.btnsearch.Text = "search";
+            this.btnsearch.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(652, 342);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(451, 303);
+            this.dataGridView1.TabIndex = 24;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1230, 703);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnsearch);
+            this.Controls.Add(this.textBoxsearchfor);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxsearched);
             this.Controls.Add(this.btnprev);
             this.Controls.Add(this.btnnext);
             this.Controls.Add(this.btnlast);
@@ -223,8 +314,12 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBoxname);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -248,6 +343,12 @@
         private System.Windows.Forms.Button btnnext;
         private System.Windows.Forms.Button btnlast;
         private System.Windows.Forms.Button btnfirst;
+        private System.Windows.Forms.ComboBox comboBoxsearched;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxsearchfor;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnsearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Common;
 
 namespace phone
 {
     public partial class Form1 : Form
     {
+        SqlConnection conn = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+        CurrencyManager cr;
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -48,6 +56,40 @@ namespace phone
         }
 
         private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn.ConnectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename =D:\C# LREARNING\FARADARS\APPS\PHONE\DATABASE1.MDF; Integrated Security = True";
+            conn.Open();
+            fillgred();
+        }
+        void fillgred(string s="Select * from tbltel")
+        {
+            cmd.CommandText = s;
+            cmd.Connection = conn;
+            da.SelectCommand = cmd;
+            da.Fill(ds,"T1");
+            dataGridView1.DataBindings.Clear();
+            dataGridView1.DataBindings.Add("datasource", ds, "T1");
+            textBoxname.DataBindings.Clear();
+            textBoxname.DataBindings.Add("text", ds, "T1.firstname");
+            textBoxfamily.DataBindings.Clear();
+            textBoxfamily.DataBindings.Add("text", ds, "T1.lastname");
+            textBoxphone.DataBindings.Clear();
+            textBoxphone.DataBindings.Add("text", ds, "T1.phoneno");
+            textBoxaddress.DataBindings.Clear();
+            textBoxaddress.DataBindings.Add("text", ds, "T1.address");
+            cr = (CurrencyManager)this.BindingContext[ds, "T1"];
+
+
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
